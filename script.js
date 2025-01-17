cc7029f0ae59ad08937f1d3a9311218bd00324ba
zethboy <yoginugraha842@gmail.com>
2025-01-17 19:12:40 +0700

    render todo

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const todoList = document.querySelector('#todo-list')
 // array todo
 let todos = []
 
+// fungsi buat render todo ke html
+function renderTodos(){
+  todoList.innerHTML = '' //buat hapus elemen sebelumnya
+  todos.forEach((todo, index)=> {
+    const li = document.querySelector('#li')
+    li.className = `todo-item ${todo.completed ? 'completed' : ''}`
+    li.innerHTML=`
+      <span>${todo.text}</span>
+      <div>
+        <button onclick = "toggleComplete(${index})">✔</button>
+        <button onclick = "deleteTodo(${index})">❌</button>
+      </div>`
+  })
+
+}
