8c911e55ab951e99341938ddceb335cb132090ec
zethboy <yoginugraha842@gmail.com>
2025-01-17 19:05:43 +0700

    add js

---
+++ script.js
@@ -0,0 +1,8 @@
+//ambil elemen
+const input = document.querySelector('#todo-input')
+const addButton = document.querySelector('#add-todo')
+const todoList = document.querySelector('#todo-list')
+
+// array todo
+let todos = []
+
